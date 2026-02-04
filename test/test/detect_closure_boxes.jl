3c3933eb50d61c4b39a942b6299774a8558e852c
WooKyoung Noh <wookay.noh@gmail.com>
2026-02-04 16:25:23 +0900

    compat Test.detect_closure_boxes

--- test/test/detect_closure_boxes.jl
+++ test/test/detect_closure_boxes.jl
@@ -2,7 +2,9 @@ module test_Test_detect_closure_boxes
 
 using Test
 
+if VERSION >= v"1.14.0-DEV.1629" # julia commit 1c9ecc21b8
 Test.detect_closure_boxes
 Test.detect_closure_boxes_all_modules
+end
 
 end # module test_Test_detect_closure_boxes
