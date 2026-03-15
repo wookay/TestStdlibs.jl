52c75718d49ac530220c8e27f76c08dba8522570
WooKyoung Noh <wookay.noh@gmail.com>
2026-03-16 08:14:05 +0900

    fix the warning of overwritten during `Test.detect_closure_boxes`

--- test/test/detect_closure_boxes.jl
+++ test/test/detect_closure_boxes.jl
@@ -9,25 +9,25 @@ Test.detect_closure_boxes_all_modules
 
 # from julia/stdlib/Test/test/runtests.jl
 module ClosureBoxRedefTest
+    function no_boxed()
+        x = 1
+        inner() = x + 1
+        (inner(), x)
+    end
+end
+
+@test ClosureBoxRedefTest.no_boxed() == (2, 1)
+@test isempty(Test.detect_closure_boxes(ClosureBoxRedefTest))
+
+Base.eval(ClosureBoxRedefTest, quote
     function boxed()
         x = 1
         inner() = (x += 1)
         (inner(), x)
     end
-end
+end)
 
 @test ClosureBoxRedefTest.boxed() == (2, 2)
 @test Test.detect_closure_boxes(ClosureBoxRedefTest)[1][2] == [:x]
-
-Base.eval(ClosureBoxRedefTest, quote
-    function boxed()
-        x = 1
-        inner() = x + 1
-        (inner(), x)
-    end
-end)
-
-@test ClosureBoxRedefTest.boxed() == (2, 1)
-@test isempty(Test.detect_closure_boxes(ClosureBoxRedefTest))
 
 end # module test_Test_detect_closure_boxes
